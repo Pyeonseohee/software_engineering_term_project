a0c5cbe0a453f9f1d190cfa5c424d2e9cbcfbf2a
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-04-29 15:37:21 +0900

    registerPage not yet

--- demoweb/src/main/frontend/src/App.js
+++ demoweb/src/main/frontend/src/App.js
@@ -1,19 +1,30 @@
 // src/main/frontend/src/App.js
 
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
+import RegisterPage from "pages/RegisterPage/RegisterPage";
 
 function App() {
-  const [hello, setHello] = useState("");
+  // const [hello, setHello] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("/api/hello")
-      .then((response) => setHello(response.data))
-      .catch((error) => console.log(error));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/hello")
+  //     .then((response) => setHello(response.data))
+  //     .catch((error) => console.log(error));
+  // }, []);
 
-  return <div>백엔드에서 가져온 데이터입니다 : {hello}</div>;
+  // return <div>백엔드에서 가져온 데이터입니다 : {hello}</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/register" component={RegisterPage} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ demoweb/src/main/frontend/src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+function RegisterPage(props) {
+  //email과 pw를 위한 state 만들기
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Name, setName] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <lable>Email</lable>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+
+        <lable>Name</lable>
+        <input type="text" value={Name} onChange={onNameHandler} />
+
+        <lable>Password</lable>
+        <input type="Password" value={Password} onChange={onPasswordHandler} />
+
+        <lable>Confirm Password</lable>
+        <input
+          type="Password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+        <button type="submit">회원가입</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
