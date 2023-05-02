1373757b0f30549a9b49dc8c2b1912b9c54f13d4
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-05-02 22:06:46 +0900

    mistake

--- demoweb/src/main/frontend/src/setproxy.js
+++ demoweb/src/main/frontend/src/setproxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://43.201.239.13:8080", // 서버 URL or localhost:설정한포트번호 인스턴스: 43.201.239.13
+      target: "http://localhost:8080", // 서버 URL or localhost:설정한포트번호 인스턴스: 43.201.239.13
       changeOrigin: true,
     })
   );
