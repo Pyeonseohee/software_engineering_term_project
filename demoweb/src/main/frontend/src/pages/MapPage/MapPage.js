2d382a0b3e620dbdb6f39e1eeb2185d684e7f624
Pyeonseohee <voiceinter@naver.com>
2023-05-05 21:30:40 +0900

    reset kakaoMap

--- demoweb/src/main/frontend/src/pages/MapPage/MapPage.js
+++ demoweb/src/main/frontend/src/pages/MapPage/MapPage.js
@@ -4,8 +4,6 @@ import React, { useEffect } from 'react';
 
 const { kakao } = window;
 
-let infowindow = new kakao.maps.InfoWindow({zIndex:1});
-
 const MapPage = () => {
 
     useEffect(() => {
@@ -15,39 +13,16 @@ const MapPage = () => {
 			level: 3
 		};
         const map = new kakao.maps.Map(container, options);
-    
-    	const {ps} = new kakao.maps.services.Places() || undefined; 
-
-        ps.keywordSearch('입력 값', placesSearchCB); 
-
-        function placesSearchCB (data, status, pagination) {
-            if (status === kakao.maps.services.Status.OK) {
-
-                let bounds = new kakao.maps.LatLngBounds();
-
-                for (let i=0; i<data.length; i++) {
-                    displayMarker(data[i]);    
-                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-                }       
-
-                map.setBounds(bounds);
-            }
-        }
-
-        function displayMarker(place) {
-            let marker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(place.y, place.x) 
-            });
-        }
     }, []);
 
+
+    
     return (
         <div id='myMap' style={{
-            width: '500px', 
-            height: '500px'
+            width: '100%', 
+            height: '800px'
         }}></div>
     );
 }
 
-export default MapPage;
+export default MapPage; 
