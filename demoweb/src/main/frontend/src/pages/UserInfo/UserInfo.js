71e6074aca70ed0e1767610ba489288c098814ad
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-05-18 16:44:28 +0900

    complete findPwd, logout, exit

--- demoweb/src/main/frontend/src/pages/UserInfo/UserInfo.js
+++ demoweb/src/main/frontend/src/pages/UserInfo/UserInfo.js
@@ -8,10 +8,10 @@ import axios from "axios";
 
 import Narvar from "../MapPage/Narvar";
 
-var userSession = ""; // 고유한 유저
 const userInfoURL = "http://localhost:8080/api/userinfo";
 const logoutURL = "http://localhost:8080/api/logout";
 const exitURL = "http://localhost:8080/api/exit";
+const changePwdURL = "http://localhost:8080/api/changepwd";
 
 function UserInfo(props) {
   const navigate = useNavigate();
@@ -19,6 +19,7 @@ function UserInfo(props) {
   const [Email, setEmail] = useState("");
   const [Name, setName] = useState("");
 
+  // 로그아웃 핸들러
   const LogoutButtonHandler = (event) => {
     new Swal({
       icon: "question",
@@ -36,7 +37,6 @@ function UserInfo(props) {
             headers: { "Content-Type": "application/json" },
           })
           .then((res) => {
-            console.log(res);
             if (res.data == "Log out completed") {
               new Swal({
                 icon: "success",
@@ -55,10 +55,12 @@ function UserInfo(props) {
     });
   };
 
+  // 탈퇴 핸들러
   const ExitButtonHandler = (event) => {
+    var tmpPwd = "";
     new Swal({
       title: "현재 비밀번호를 입력하세요",
-      input: "text",
+      input: "password",
       inputAttributes: { autocapitalize: "off" },
       showCancelButton: true,
       cancelButtonText: "취소",
@@ -67,23 +69,113 @@ function UserInfo(props) {
         return password;
       },
     }).then((result) => {
-      if (result.isConfirmed) {
-        console.log(result.value);
-      } else {
-        console.log("Dont");
-      }
+      tmpPwd = result.value;
+      new Swal({
+        icon: "question",
+        title: "정말 탈퇴하시겠습니까?",
+        showCancelButton: true,
+        cancelButtonText: "아니요",
+        confirmButtonText: "네",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const data = {
+            session: userSession,
+            pw: tmpPwd,
+          };
+          axios
+            .post(exitURL, JSON.stringify(data), {
+              headers: { "Content-Type": "application/json" },
+            })
+            .then((res) => {
+              if (res.data == "Password Incorrect") {
+                new Swal({
+                  icon: "error",
+                  title: "현재 비밀번호와\n일치하지 않습니다.",
+                });
+              } else if (res.data == "Sign out succeeded") {
+                new Swal({
+                  icon: "success",
+                  title: "탈퇴되었습니다.",
+                }).then(function () {
+                  navigate("/");
+                });
+              } else {
+                new Swal({
+                  icon: "warning",
+                  title: "유효하지 않은 세션입니다.",
+                });
+              }
+            });
+        }
+      });
     });
   };
 
+  // 비밀번호 변경 핸들러
   const ChangedPwdButtonHandler = (event) => {
-    console.log("test");
+    new Swal({
+      title: "비밀번호 변경",
+      html:
+        "<br/>현재 비밀번호 입력" +
+        '<input type="password" id="swal-input1" class="swal2-input"><br/>' +
+        "<br/>새로운 비밀번호 입력" +
+        '<input type="password"id="swal-input2" class="swal2-input"><br/>' +
+        "<br/>새로운 비밀번호 확인" +
+        '<input type="password" id="swal-input3" class="swal2-input">',
+      confirmButtonText: "변경",
+      showCancelButton: true,
+      cancelButtonText: "취소",
+      preConfirm: () => {
+        return [
+          document.getElementById("swal-input1").value,
+          document.getElementById("swal-input2").value,
+          document.getElementById("swal-input3").value,
+        ];
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const data = {
+          session: userSession,
+          pw: result.value[0],
+          newPw: result.value[1],
+          chkPw: result.value[2],
+        };
+        axios
+          .post(changePwdURL, JSON.stringify(data), {
+            headers: { "Content-Type": "application/json" },
+          })
+          .then((res) => {
+            if (res.data === "Password Updated") {
+              new Swal({
+                icon: "success",
+                title: "비밀번호가 변경되었습니다!",
+              });
+            } else if (res.data == "Password Incorrect") {
+              new Swal({
+                icon: "error",
+                title: "현재 비밀번호와\n일치하지 않습니다.",
+              });
+            } else if (res.data === "New Password doesn't matches") {
+              new Swal({
+                icon: "error",
+                title: "새로운 비밀번호가 서로 \n일치하지 않습니다.",
+              });
+            } else {
+              new Swal({
+                icon: "warning",
+                title: "유효하지 않은 세션입니다.",
+              });
+            }
+          });
+      }
+    });
   };
 
   const location = useLocation();
   const UserInfo = { ...location.state };
   useEffect(() => {
     setUserSession(UserInfo.userSession);
-  }, []);
+  });
 
   // userInfo에게 보낼 데이터
   const data = {
