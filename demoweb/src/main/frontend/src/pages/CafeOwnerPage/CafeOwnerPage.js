fd351390c3951c814d397925597000d82bcda36c
youhyunwoo99 <casshyunwoo@naver.com>
2023-05-24 21:16:01 +0900

    menus api추가

--- demoweb/src/main/frontend/src/pages/CafeOwnerPage/CafeOwnerPage.js
+++ demoweb/src/main/frontend/src/pages/CafeOwnerPage/CafeOwnerPage.js
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import Narvar from "../MapPage/Narvar";
+
+//const StoreInfoURL = "http://localhost:8080/api/storeinfo";
 
 function CafeOwnerPage() {
   // 매장 정보 상태 관리
@@ -8,7 +12,11 @@ function CafeOwnerPage() {
     phoneNumber: ''
   });
   const [stores, setStores] = useState([]);
-  const [showStoreName, setShowStoreName] = useState(false);
+
+  useEffect(() => {
+    // 초기 매장 데이터 불러오기
+    fetchStores();
+  }, []);
 
   // 매장 정보 변경
   const handleChange = (event) => {
@@ -21,65 +29,93 @@ function CafeOwnerPage() {
     if (store.name.trim() === '' || store.address.trim() === '' || store.phoneNumber.trim() === '') {
       return; // 필수 필드가 비어있으면 매장 추가하지 않음
     }
-    //매장 정보를 서버에 저장하는 로직을 구현해야함.
-    setStores([...stores, store]);
-    setStore({ name: '', address: '', phoneNumber: '' });
-    setShowStoreName(false);
+
+    // 매장 정보를 서버로 전송
+    axios.post("/api/storeinfo", store)
+      .then(response => {
+        // 새로운 매장으로 stores 상태 업데이트
+        setStores([...stores, response.data]);
+        setStore({ name: '', address: '', phoneNumber: '' });
+      })
+      .catch(error => {
+        console.error("매장 추가 에러:", error);
+      });
   };
 
   // 매장 삭제
   const handleDeleteStore = (index) => {
-    const newStores = [...stores];
-    newStores.splice(index, 1);
-    setStores(newStores);
+    const storeId = stores[index]._id;
+
+    // 매장 정보를 서버에서 삭제
+    axios.delete(`/api/storeinfo/${storeId}`)
+      .then(() => {
+        // 매장 삭제 후 업데이트된 매장 목록을 불러옴
+        fetchStores();
+      })
+      .catch(error => {
+        console.error("매장 삭제 에러:", error);
+      });
   };
 
+  // 매장 목록 불러오기
+  const fetchStores = () => {
+    axios.get("/api/storeinfo")
+      .then(response => {
+        setStores(response.data);
+      })
+      .catch(error => {
+        console.error("매장 목록 불러오기 에러:", error);
+      });
+  };
 
   return (
-    <div className="Main" style={{ margin: 'auto', width: '700px' }}>
-      <div className="d-grid gap-2">
-        <div>
-          <h1>매장 관리</h1>
-          <form>
-            <label>
-              매장 이름:
-              <input
-                type="text"
-                name="name"
-                value={store.name}
-                onChange={handleChange}
-              />
-            </label>
-            <br />
-            <label>
-              매장 주소:
-              <input
-                type="text"
-                name="address"
-                value={store.address}
-                onChange={handleChange}
-              />
-            </label>
-            <br />
-            <label>
-              전화번호:
-              <input
-                type="text"
-                name="phoneNumber"
-                value={store.phoneNumber}
-                onChange={handleChange}
-              />
-            </label>
-          </form>
-          <button onClick={handleAddStore}>매장 추가</button>
-          <ul>
-            {stores.map((s, index) => (
-              <li key={index}>
-                {s.name}{' '}
-                <button onClick={() => handleDeleteStore(index)}>삭제</button>
-              </li>
-            ))}
-          </ul>
+    <div>
+      <Narvar></Narvar>
+      <div className="Main" style={{ margin: 'auto', width: '700px' }}>
+        <div className="d-grid gap-2">
+          <div>
+            <h1>매장 관리</h1>
+            <form>
+              <label>
+                매장 이름:
+                <input
+                  type="text"
+                  name="name"
+                  value={store.name}
+                  onChange={handleChange}
+                />
+              </label>
+              <br />
+              <label>
+                매장 주소:
+                <input
+                  type="text"
+                  name="address"
+                  value={store.address}
+                  onChange={handleChange}
+                />
+              </label>
+              <br />
+              <label>
+                전화번호:
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  value={store.phoneNumber}
+                  onChange={handleChange}
+                />
+              </label>
+            </form>
+            <button onClick={handleAddStore}>매장 추가</button>
+            <ul>
+              {stores.map((s, index) => (
+                <li key={index}>
+                  {s.name}{' '}
+                  <button onClick={() => handleDeleteStore(index)}>삭제</button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- demoweb/src/main/frontend/src/pages/ProductManagement/ProductManagement.js
+++ demoweb/src/main/frontend/src/pages/ProductManagement/ProductManagement.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import Narvar from "../MapPage/Narvar";
+
+//const menusURL = "http://localhost:8080/api/menus";
 
 function ProductManagement() {
   // 메뉴 정보 상태 관리
@@ -8,63 +12,99 @@ function ProductManagement() {
   });
   const [menus, setMenus] = useState([]);
 
+  useEffect(() => {
+    // 초기 메뉴 데이터 불러오기
+    fetchMenus();
+  }, []);
+
   // 메뉴 정보 변경
   const handleChange = (event) => {
     const { name, value } = event.target;
     setMenu({ ...menu, [name]: value });
   };
 
-  // 메뉴 추가
+  // 메뉴 추가 및 저장
   const handleAddMenu = () => {
     if (menu.name.trim() === "" || menu.cost.trim() === "") {
       return; // 필수 필드가 비어있으면 메뉴 추가하지 않음
     }
-    setMenus([...menus, menu]);
-    setMenu({ name: "", cost: "" });
+
+    // 메뉴 정보를 서버로 전송
+    axios.post("/api/menus", menu)
+      .then(response => {
+        // 새로운 메뉴로 menus 상태 업데이트
+        setMenus([...menus, response.data]);
+        setMenu({ name: "", cost: "" });
+      })
+      .catch(error => {
+        console.error("메뉴 추가 에러:", error);
+      });
   };
 
   // 메뉴 삭제
   const handleDeleteMenu = (index) => {
-    const newMenus = [...menus];
-    newMenus.splice(index, 1);
-    setMenus(newMenus);
+    const menuId = menus[index]._id;
+
+    // 메뉴 정보를 서버에서 삭제
+    axios.delete(`/api/menus/${menuId}`)
+      .then(() => {
+        // 메뉴 삭제 후 업데이트된 메뉴 목록을 불러옴
+        fetchMenus();
+      })
+      .catch(error => {
+        console.error("메뉴 삭제 에러:", error);
+      });
+  };
+
+  // 메뉴 목록 불러오기
+  const fetchMenus = () => {
+    axios.get("/api/menus")
+      .then(response => {
+        setMenus(response.data);
+      })
+      .catch(error => {
+        console.error("메뉴 목록 불러오기 에러:", error);
+      });
   };
 
   return (
-    <div className="Main" style={{ margin: "auto", width: "700px" }}>
-      <div className="d-grid gap-2">
-        <div>
-          <h1>메뉴 관리</h1>
-          <form>
-            <label>
-              메뉴 이름:
-              <input
-                type="text"
-                name="name"
-                value={menu.name}
-                onChange={handleChange}
-              />
-            </label>
-            <br />
-            <label>
-              메뉴 가격:
-              <input
-                type="text"
-                name="cost"
-                value={menu.cost}
-                onChange={handleChange}
-              />
-            </label>
-          </form>
-          <button onClick={handleAddMenu}>메뉴 추가</button>
-          <ul>
-            {menus.map((m, index) => (
-              <li key={index}>
-                {m.name}{" "}
-                <button onClick={() => handleDeleteMenu(index)}>삭제</button>
-              </li>
-            ))}
-          </ul>
+    <div>
+      <Narvar></Narvar>
+      <div className="Main" style={{ margin: "auto", width: "700px" }}>
+        <div className="d-grid gap-2">
+          <div>
+            <h1>메뉴 관리</h1>
+            <form>
+              <label>
+                메뉴 이름:
+                <input
+                  type="text"
+                  name="name"
+                  value={menu.name}
+                  onChange={handleChange}
+                />
+              </label>
+              <br />
+              <label>
+                메뉴 가격:
+                <input
+                  type="text"
+                  name="cost"
+                  value={menu.cost}
+                  onChange={handleChange}
+                />
+              </label>
+            </form>
+            <button onClick={handleAddMenu}>메뉴 추가</button>
+            <ul>
+              {menus.map((m, index) => (
+                <li key={m._id}>
+                  {m.name} {m.cost}{" "}
+                  <button onClick={() => handleDeleteMenu(index)}>삭제</button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
